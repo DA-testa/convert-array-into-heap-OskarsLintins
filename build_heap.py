2b0e25fec702fde4e69dd4a3cd8bb42d2d394eeb
OskarsLintins <124681718+OskarsLintins@users.noreply.github.com>
2023-03-16 14:44:25 +0000

    Oskars Lintins 221RDB434

--- build_heap.py
+++ build_heap.py
@@ -3,44 +3,38 @@ import os;
 
 def build_heap(data):
     swaps = []
-    length = len(data)
-    for i in range(length//2-1,-1,1):
+    n = len(data)
+    for i in range(n//2-1,-1,-1):
         j = i
         while True:
-            leftChild = 2*i + 1
-            rightChild = 2*i + 2
-            if rightChild <= n-1:
+            leftChild = 2 * i + 1
+            rightChild = 2 * i + 2
+            if rightChild < n and data[rightChild] < data[j]:
                 j = rightChild
-            elif leftChild <= n-1:
+            if leftChild < n and data[leftChild] < data[j]:
                 j = leftChild
             if i != j:
                 data[i], data[j] = data[j], data[i]
-                swaps.append(i,j)
+                swaps.append((i,j))
                 i = j
             else:
                 break
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
     return swaps
-
 
 def main():
     
     word = input()
+
     if 'I' in word:
         n = int(input())
         data = list(map(int, input().split()))
+
     elif 'F' in word:
         filename = input().strip()
-        filepath = os.path.join(os.getcwd(),'test/' + filename)
-        if filename.endswith("a"):
-            return
-        else:
-            with open(filepath, 'r') as fn:
-                n = int(fn.readline())
-                data = list(map(int, fn.readline().split()))
+        ##filepath = os.path.join(os.getcwd(),'test/' + filename)
+        with open('/workspaces/convert-array-into-heap-OskarsLintins/tests/' + filename, 'r') as fn:
+            n = int(fn.readline())
+            data = list(map(int, fn.readline().split()))
 
     assert len(data) == n
     swaps = build_heap(data)
